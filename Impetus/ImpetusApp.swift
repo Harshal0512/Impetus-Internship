54d1b131f07dc7201967f639c9680a8953623989
Harshal Kulkarni <harshal.kulkarni0512@gmail.com>
2022-04-21 09:32:46 +0530

    Initial Commit

---
+++ Impetus/ImpetusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImpetusApp.swift
+//  Impetus
+//
+//  Created by Harshal Kulkarni on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ImpetusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
