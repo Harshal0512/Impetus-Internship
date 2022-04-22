d66926d7c45f0c9f8ac280b7df45cd054a41b6d1
Harshal Kulkarni <harshal.kulkarni0512@gmail.com>
2022-04-22 13:01:50 +0530

    Fixed UI Bug, Added Price Text & Edit Button

--- Impetus/Views/ContentView.swift
+++ Impetus/Views/ContentView.swift
@@ -6,38 +6,86 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import AlertToast
+
+
+class AppViewModel: ObservableObject {
+    
+    let auth = Auth.auth()
+    
+    @Published var signedIn = false
+    @Published var wrongCredentialsToast = false
+    @Published var authenticatedToast = false
+    
+    var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+    
+    func signIn(email: String, password: String) {
+        auth.signIn(withEmail: email, password: password) { [weak self] result, error in
+            guard result != nil, error == nil else {
+                self?.wrongCredentialsToast.toggle()
+                return
+            }
+            
+            //success
+            self?.authenticatedToast = true
+            DispatchQueue.main.async {
+                self?.signedIn = true
+            }
+        }
+    }
+    
+    
+    func signUp(email: String, password: String) {
+        auth.createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard result != nil, error == nil else {
+                return
+            }
+            
+            //success
+            DispatchQueue.main.async {
+                self?.signedIn = true
+                self?.authenticatedToast = true
+            }
+        }
+    }
+    
+    func logout() {
+        do {
+            try auth.signOut()
+            
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.signedIn = false
+            self.authenticatedToast = false
+        }
+    }
+    
+}
+
 
 struct ContentView: View {
     
-//    @State var products = [Product]()
+    //    @State var products = [Product]()
+    @EnvironmentObject var viewModel: AppViewModel
     
     var body: some View {
-        ZStack{
-            Color("Bg")
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack (alignment: .leading){
-                AppBarView()
-                
-//                Spacer()
-//                    .frame(height: 10.0)
-                
-                TagLineView()
-                    .padding(.leading)
-                
-//                SearchView()
-//
-//                Spacer()
-//                    .frame(height: 20.0)
-//
-//                ThreeButtonView()
-                
-//                Spacer()
-//                    .frame(height: 550.0)
-                    
-                ListView_Products()
-                
+        NavigationView{
+            if viewModel.signedIn {
+                DashboardMainView()
+                    .navigationBarHidden(true)
+            } else {
+                LoginView()
             }
+        }
+        .onAppear{
+            viewModel.signedIn = viewModel.isSignedIn
         }
     }
 }
@@ -52,9 +100,13 @@ struct ContentView_Previews: PreviewProvider {
 
 
 struct AppBarView: View {
+    @EnvironmentObject var viewModel: AppViewModel
+    
     var body: some View {
         HStack{
-            Button(action: {}) {
+            Button(action: {
+                viewModel.logout()
+            }) {
                 Image("menu")
                     .padding()
                     .background(Color(.white))
@@ -175,17 +227,99 @@ struct ListView_Products: View {
         NavigationView {
             List(products) { product in
                 NavigationLink {
-                    ProductDetail()
+                    ProductDetail(product: product)
                 } label: {
                     ProductRow(product: product)
                 }
             }
-//            .ignoresSafeArea()
-//            .onAppear(perform: {
-//                UITableView.appearance().contentInset.top = -35})
-//            .navigationTitle("All Products")
+            //            .ignoresSafeArea()
+            //            .onAppear(perform: {
+            //                UITableView.appearance().contentInset.top = -35})
+            //            .navigationTitle("All Products")
             .navigationBarHidden(true)
         }
     }
 }
 
+
+struct DashboardMainView: View {
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        ZStack{
+            Color("Bg")
+                .edgesIgnoringSafeArea(.all)
+
+            VStack (alignment: .leading){
+                AppBarView()
+                
+                TagLineView()
+                    .padding(.leading)
+                
+                ListView_Products()
+//                    .onAppear(){
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+//                            self.viewModel.authenticatedToast = false
+//                        }
+//
+//                    }
+//                    .toast(isPresenting: $viewModel.authenticatedToast){
+//                        AlertToast(type: .regular, title: "Login Successful", subTitle: "Welcome \(viewModel.auth.currentUser?.displayName ?? "Back!")")
+//                    }
+            }
+        }
+    }
+}
+
+
+struct LoginView: View {
+    @State var email = ""
+    @State var password = ""
+    @State private var alertWrongCredentials = false
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Email Address", text: $email)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding(15)
+                    .background(Color(.secondarySystemBackground))
+                    .padding(.bottom, 5)
+                
+                SecureField("Password", text: $password)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding(15)
+                    .background(Color(.secondarySystemBackground))
+                    .padding(.bottom, 15)
+                
+                Button(action: {
+                    guard !email.isEmpty, !password.isEmpty else {
+                        return
+                    }
+                    
+                    viewModel.signIn(email: email, password: password)
+                }, label: { Text("Sign In")
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 16, weight: .bold, design: .default))
+                })
+                .toast(isPresenting: $viewModel.wrongCredentialsToast){
+                    AlertToast(type: .error(Color.black), title: "Try Again", subTitle: "Wrong Email/Password")
+                }
+                
+                
+                Spacer()
+                    .frame(height: 100)
+            }
+        }
+        .navigationTitle("Sign In")
+        .padding()
+    }
+}
--- Impetus/Views/ProductDetail.swift
+++ Impetus/Views/ProductDetail.swift
@@ -6,15 +6,75 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ProductDetail: View {
+    var product: Product
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                Color("Bg")
+                    .edgesIgnoringSafeArea(.all)
+                ScrollView{
+                    VStack {
+                        KFImage(URL(string: product.image)!)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+//                          .edgesIgnoringSafeArea(.top)
+                    
+                        VStack(alignment: .leading) {
+                            Text(product.title)
+                                .font(.title)
+                                .fontWeight(.bold)
+                        
+                            HStack {
+                                Text("Category: \(product.category.capitalized)")
+                                    .opacity(0.5)
+                                Spacer()
+                            }
+                            .padding(.bottom)
+                        
+                            Text("Description")
+                                .fontWeight(.medium)
+                                .padding(.top, 8)
+                                .padding(.bottom,4)
+                        
+                            Text(product.description)
+                                .lineSpacing(8.0)
+                                .opacity(0.6)
+                        }
+                        .padding()
+                        .padding(.top)
+                        .background(Color.white)
+                    
+                        HStack{
+                            Text("$\(String(format: "%g", product.price))")
+                                .font(Font.system(size:30))
+                                .foregroundColor(Color.white)
+                            
+                            Spacer()
+                            
+                            Button (action: {},  label: {
+                                Label("Edit Item", systemImage: "pencil")
+                                    .padding()
+                                    .padding(.horizontal)
+                                    .background(Color.white)
+                                    .cornerRadius(10.0)
+                            })
+                        }
+                        .padding()
+                        .background(Color("Primary"))
+                        .frame(maxHeight: .infinity, alignment: .bottom)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ProductDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ProductDetail()
+        ProductDetail(product: products[0])
     }
 }
